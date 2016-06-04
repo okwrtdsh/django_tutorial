c8d3923d1bc15cb31e8b5f3ed2dca9c7d144cbf5
okwrtdsh <okwrtdsh@gmail.com>
2016-06-04 22:43:35 +0900

    TOPページの作成

--- src/tutorial/urls.py
+++ src/tutorial/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.shortcuts import render
 
 
 admin.autodiscover()
 
 urlpatterns = [
+    url(r'^$', lambda request: render(request, "index.html"), name="index"),
     url(r'^todo/', include('tutorial.todo.urls', namespace="todo")),
     url(r'^master/', include('tutorial.master.urls', namespace="master")),
     url(r'^admin/', include('tutorial.site_admin.urls', namespace="site_admin")),
