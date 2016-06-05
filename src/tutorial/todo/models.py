faf4c3f7222014f269c7adf36c950998948e5070
okwrtdsh <okwrtdsh@gmail.com>
2016-06-05 20:06:56 +0900

    ToDo一覧

--- src/tutorial/todo/models.py
+++ src/tutorial/todo/models.py
@@ -53,3 +53,6 @@ class ToDo(ModelBase):
         self.completed = timezone.now()
         self.save()
 
+    def get_categories_display(self):
+        return " ".join([c.name for c in self.categories.all()])
+
--- src/tutorial/todo/urls.py
+++ src/tutorial/todo/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from tutorial.todo.views import ToDoUserCreateView, ToDoCreateView
+from tutorial.todo.views import ToDoUserCreateView, ToDoCreateView,\
+     ToDoListView
 
 urlpatterns = [
+    url(r"^$", ToDoListView.as_view(), name="list"),
     url(r"^create/$", ToDoCreateView.as_view(), name="create"),
     url(r"^user/create/$", ToDoUserCreateView.as_view(), name="user_create"),
 ]
