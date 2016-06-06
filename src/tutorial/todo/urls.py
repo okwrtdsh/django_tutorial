a5343a876433e017d9eaacdc9ca3811dd4713aaf
okwrtdsh <okwrtdsh@gmail.com>
2016-06-06 21:13:09 +0900

    ToDo詳細

--- src/tutorial/todo/urls.py
+++ src/tutorial/todo/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from tutorial.todo.views import ToDoUserCreateView, ToDoCreateView,\
-     ToDoListView
+     ToDoListView, ToDoDetailView
 
 urlpatterns = [
     url(r"^$", ToDoListView.as_view(), name="list"),
+    url(r"^(?P<todo_id>\d+)/$", ToDoDetailView.as_view(), name="detail"),
     url(r"^create/$", ToDoCreateView.as_view(), name="create"),
     url(r"^user/create/$", ToDoUserCreateView.as_view(), name="user_create"),
 ]
